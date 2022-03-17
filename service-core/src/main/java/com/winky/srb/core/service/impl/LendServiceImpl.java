101888984493acf431b4551dd6c5d67f46129212
Winky <winky1230@qq.com>
2022-03-17 17:44:24 +0800

    feat: 查询新标

--- service-core/src/main/java/com/winky/srb/core/service/impl/LendServiceImpl.java
+++ service-core/src/main/java/com/winky/srb/core/service/impl/LendServiceImpl.java
@@ -1,19 +1,32 @@
 package com.winky.srb.core.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.winky.srb.core.enums.LendStatusEnum;
+import com.winky.srb.core.mapper.BorrowerMapper;
+import com.winky.srb.core.mapper.DictMapper;
 import com.winky.srb.core.pojo.entity.BorrowInfo;
+import com.winky.srb.core.pojo.entity.Borrower;
 import com.winky.srb.core.pojo.entity.Lend;
 import com.winky.srb.core.mapper.LendMapper;
 import com.winky.srb.core.pojo.vo.BorrowInfoApprovalVO;
+import com.winky.srb.core.pojo.vo.BorrowerDetailVO;
+import com.winky.srb.core.service.BorrowerService;
+import com.winky.srb.core.service.DictService;
 import com.winky.srb.core.service.LendService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.winky.srb.core.util.LendNoUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -25,7 +38,14 @@ import java.time.format.DateTimeFormatter;
  */
 @Service
 public class LendServiceImpl extends ServiceImpl<LendMapper, Lend> implements LendService {
+    @Autowired
+    DictService dictService;
+    @Autowired
+    BorrowerMapper borrowerMapper;
+    @Autowired
+    BorrowerService borrowerService;
 
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public void createLend(BorrowInfoApprovalVO borrowInfoApprovalVO, BorrowInfo borrowInfo) {
         Lend lend = new Lend();
@@ -46,7 +66,7 @@ public class LendServiceImpl extends ServiceImpl<LendMapper, Lend> implements Le
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate lendStartDate = LocalDate.parse(borrowInfoApprovalVO.getLendStartDate(), dtf);
         lend.setLendStartDate(lendStartDate);
-        //结束日期
+        //结束日期 LocalDate里面的plusMonths就是加一个月
         LocalDate lendEndDate = lendStartDate.plusMonths(borrowInfo.getPeriod());
         lend.setLendEndDate(lendEndDate);
 
@@ -65,9 +85,46 @@ public class LendServiceImpl extends ServiceImpl<LendMapper, Lend> implements Le
         lend.setStatus(LendStatusEnum.INVEST_RUN.getStatus());
         //审核时间
         lend.setCheckTime(LocalDateTime.now());
-        //审核人
+        //审核人（这个项目不做了）
         lend.setCheckAdminId(1L);
 
         baseMapper.insert(lend);
+    }
+
+    @Override
+    public List<Lend> selectList() {
+        List<Lend> lendList = baseMapper.selectList(null);
+        lendList.forEach(lend -> {
+            String returnMethod = dictService.getNameByParentDictCodeAndValue("returnMethod", lend.getReturnMethod());
+            String status = LendStatusEnum.getMsgByStatus(lend.getStatus());
+            lend.getParam().put("returnMethod",returnMethod);
+            lend.getParam().put("status",status);
+
+        });
+        return lendList;
+    }
+
+    @Override
+    public Map<String, Object> getLendDetail(Long id) {
+
+        //组装查询lend
+        Lend lend = baseMapper.selectById(id);
+        String returnMethod = dictService.getNameByParentDictCodeAndValue("returnMethod", lend.getReturnMethod());
+        String status = LendStatusEnum.getMsgByStatus(lend.getStatus());
+        lend.getParam().put("returnMethod",returnMethod);
+        lend.getParam().put("status",status);
+        //根据user_id获取借款人对象
+        QueryWrapper<Borrower> borrowerQueryWrapper = new QueryWrapper<Borrower>();
+        borrowerQueryWrapper.eq("user_id", lend.getUserId());
+        Borrower borrower = borrowerMapper.selectOne(borrowerQueryWrapper);
+        //组装借款人对象
+        BorrowerDetailVO borrowerDetailVO = borrowerService.getBorrowerDetailVOById(borrower.getId());
+
+        //组装集合结果
+        Map<String,Object> result = new HashMap<>();
+        result.put("lend",lend);
+        result.put("borrower",borrowerDetailVO);
+
+        return result;
     }
 }
